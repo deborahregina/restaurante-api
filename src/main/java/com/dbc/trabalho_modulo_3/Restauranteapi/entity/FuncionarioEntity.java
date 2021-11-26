0c895cbbf77e565c01f1d1229116ff47490f3886
Adriele Beatriz Santos <adriele.beatriz98@outlook.com>
2021-11-26 17:16:53 -0300

    notações no entity

--- src/main/java/com/dbc/trabalho_modulo_3/Restauranteapi/entity/FuncionarioEntity.java
+++ src/main/java/com/dbc/trabalho_modulo_3/Restauranteapi/entity/FuncionarioEntity.java
@@ -4,6 +4,10 @@ import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
@@ -11,10 +15,19 @@ import java.util.Date;
 public class FuncionarioEntity {
     @Id
     private String id;
-
+    @NotEmpty
+    @NotBlank
     private String nome;
+    @NotEmpty
+    @NotBlank
     private String cargo;
+    @NotNull
+    @NotEmpty
+    @Size(max = 13, message = "Não pode possuir mais que 13 caracteres")
     private String telefone;
+    @NotEmpty
+    @NotNull
+    @Size(max = 8,min = 8,message = "mais de 8 caracteres")
     private String cep;
 //    private Date dataNascimento;
 
